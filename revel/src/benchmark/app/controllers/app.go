d2a3ba04fa9d8957041daf959ce22a5f27fb4837
Rob Figueiredo <robfig@gmail.com>
2013-06-05 10:05:38 -0400

    Stop server-side fanout
    Conflicts:
    	revel/src/benchmark/app/controllers/app.go

--- revel/src/benchmark/app/controllers/app.go
+++ revel/src/benchmark/app/controllers/app.go
@@ -6,7 +6,6 @@ import (
 	"math/rand"
 	"runtime"
 	"sort"
-	"sync"
 )
 
 type MessageStruct struct {
@@ -64,20 +63,12 @@ func (c App) Db(queries int) revel.Result {
 	}
 
 	ww := make([]World, queries)
-	var wg sync.WaitGroup
-	wg.Add(queries)
-	acquire(queries)
 	for i := 0; i < queries; i++ {
-		go func(i int) {
-			err := db.Jet.Query(WorldSelect, rand.Intn(WorldRowCount)+1).Rows(&ww[i])
-			if err != nil {
-				revel.ERROR.Fatalf("Db/WorldSelect2 error: %v", err)
-			}
-			wg.Done()
-		}(i)
+		err := db.Jet.Query(WorldSelect, rand.Intn(WorldRowCount)+1).Rows(&ww[i])
+		if err != nil {
+			revel.ERROR.Fatalf("Db/WorldSelect2 error: %v", err)
+		}
 	}
-	wg.Wait()
-	release(queries)
 	return c.RenderJson(ww)
 }
 
@@ -95,27 +86,17 @@ func (c App) Update(queries int) revel.Result {
 		return c.RenderJson(&w)
 	}
 
-	var (
-		ww = make([]World, queries)
-		wg sync.WaitGroup
-	)
-	wg.Add(queries)
-	acquire(queries)
+	ww := make([]World, queries)
 	for i := 0; i < queries; i++ {
-		go func(i int) {
-			err := db.Jet.Query(WorldSelect, rand.Intn(WorldRowCount)+1).Rows(&ww[i])
-			if err != nil {
-				revel.ERROR.Fatalf("Error scanning world row: %v", err)
-			}
-			ww[i].RandomNumber = uint16(rand.Intn(WorldRowCount) + 1)
-			if err = db.Jet.Query(WorldUpdate, ww[i].RandomNumber, ww[i].Id).Run(); err != nil {
-				revel.ERROR.Fatalf("Update/WorldUpdate2 error: %v", err)
-			}
-			wg.Done()
-		}(i)
+		err := db.Jet.Query(WorldSelect, rand.Intn(WorldRowCount)+1).Rows(&ww[i])
+		if err != nil {
+			revel.ERROR.Fatalf("Error scanning world row: %v", err)
+		}
+		ww[i].RandomNumber = uint16(rand.Intn(WorldRowCount) + 1)
+		if err = db.Jet.Query(WorldUpdate, ww[i].RandomNumber, ww[i].Id).Run(); err != nil {
+			revel.ERROR.Fatalf("Update/WorldUpdate2 error: %v", err)
+		}
 	}
-	wg.Wait()
-	release(queries)
 	return c.RenderJson(ww)
 }
 
@@ -138,43 +119,3 @@ func (s Fortunes) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
 type ByMessage struct{ Fortunes }
 
 func (s ByMessage) Less(i, j int) bool { return s.Fortunes[i].Message < s.Fortunes[j].Message }
-
-// Utility for limiting in-flight queries to MaxConnectionLimit
-
-var (
-	conns   int
-	connMu  = &sync.Mutex{}
-	waiting []*Wait
-)
-
-type Wait struct {
-	n    int
-	cond *sync.Cond
-}
-
-func acquire(n int) {
-	connMu.Lock()
-	var w *Wait
-	if conns+n > MaxConnectionCount {
-		w = &Wait{n, sync.NewCond(connMu)}
-		waiting = append(waiting, w)
-		w.cond.Wait()
-	} else {
-		conns += n
-	}
-	connMu.Unlock()
-}
-
-func release(n int) {
-	var w *Wait
-	connMu.Lock()
-	conns -= n
-	if len(waiting) > 0 && waiting[0].n <= MaxConnectionCount-conns {
-		w, waiting = waiting[0], waiting[1:]
-		conns += w.n
-	}
-	connMu.Unlock()
-	if w != nil {
-		w.cond.Signal()
-	}
-}
